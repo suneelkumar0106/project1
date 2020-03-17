155b9a9777ec99b66ee3b5bdb6edb752d55b4c29
suneelinno <k.sunilkumar0106@gmail.com>
2020-03-17 10:42:53 +0530

    new angular doc

---
+++ index.php
@@ -0,0 +1,17 @@
+{
+"record":[
+    {
+      "Name":"suneel kumar",
+      "City":"nellore",
+      "Country":"india"  
+    },
+    {
+        "Name":"gireesh kumar",
+      "City":"nigeria",
+      "Country":"south africa"   
+    }
+
+    ]
+
+
+}
